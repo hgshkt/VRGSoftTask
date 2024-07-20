d27e8cb501760fa4e452777e32ef8670ba41bda7
hgshkt <nichtojestvozxc@gmail.com>
2024-07-20 16:01:48 +0300

    Children.toDomain() map fixing

--- data/src/main/java/com/hgshkt/data/mapper/publication.kt
+++ data/src/main/java/com/hgshkt/data/mapper/publication.kt
@@ -3,8 +3,10 @@ package com.hgshkt.data.mapper
 import com.hgshkt.data.api.publication.model.PublicationPageJson
 import com.hgshkt.domain.model.Publication
 
-fun PublicationPageJson.Data.Children.ChildrenData.toDomain(): Publication {
-    return Publication(
-        id = name!!
-    )
+fun PublicationPageJson.Data.Children.toDomain(): Publication {
+    with(childrenData!!) {
+        return Publication(
+            id = name!!
+        )
+    }
 }
