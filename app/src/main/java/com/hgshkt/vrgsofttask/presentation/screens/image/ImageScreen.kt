431d490533b79587bf551e1211b6fba819f73878
hgshkt <nichtojestvozxc@gmail.com>
2024-07-21 06:59:44 +0300

    image screen impl

--- app/src/main/java/com/hgshkt/vrgsofttask/presentation/screens/image/ImageScreen.kt
+++ app/src/main/java/com/hgshkt/vrgsofttask/presentation/screens/image/ImageScreen.kt
@@ -1,10 +1,17 @@
 package com.hgshkt.vrgsofttask.presentation.screens.image
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import coil.compose.AsyncImage
 
 @Composable
 fun ImageScreen(
-
+    imageUrl: String
 ) {
-
+    AsyncImage(
+        model = imageUrl,
+        contentDescription = "Publication image",
+        modifier = Modifier.fillMaxSize()
+    )
 }
